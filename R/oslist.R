b49302db048ee1f9aa0d4fbe961cff1f75edbe84
witusj <wfth69@gmail.com>
2017-11-25 17:52:07 +0100

    Update

--- R/oslist.R
+++ R/oslist.R
@@ -1,5 +1,6 @@
 library(rvest)
 library(dplyr)
+library(bubbles)
 
 scraping_wiki <- read_html("https://en.wikipedia.org/wiki/List_of_free_and_open-source_software_packages")
 
@@ -9,6 +10,12 @@ scrapedDataRaw <- scraping_wiki %>%
   unique()
 
 scrapedDataClean <- scrapedDataRaw[129:568] %>% as.data.frame()
+names(scrapedDataClean) <- "Names"
+
+l <- dim(scrapedDataClean)[1]
+with(scrapedDataClean, bubbles(value = runif(l, min = 10, max = 25), label = "",
+        color = rainbow(l, alpha=NULL)[sample(l)]
+))
 
 
 
